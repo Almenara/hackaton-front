61d52f605fbf7058d685b258683fed30a56df354
Roger Miret <r.mir.min@gmail.com>
2023-06-20 16:50:28 +0200

    list-component: addZone, rejectZone

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService } from 'src/app/services/list.service';
-import { Observable, of } from 'rxjs';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 import { Zone, User } from 'src/app/models/interfaces';
 import { MainService } from 'src/app/services/main.service';
 
@@ -12,14 +12,33 @@ import { MainService } from 'src/app/services/main.service';
 export class ListComponent implements OnInit {
 
   zonesList$: Observable<Zone[]|null>;
-  user$: Observable<User|null>;
+  user$: Observable<User | null>;
+  
+  updateUser = new BehaviorSubject<User|null>(null);
+  updateUser$ = this.updateUser.asObservable();
 
   constructor(private listService: ListService, private mainService: MainService) {
+    //Current user from user.service, to display user features
+    // this.user$ = this.updateUser$.pipe(switchMap(() => this.userService.getUser())); o this.userService.user$
     this.user$ = of({ name: 'Jaume', surname: 'Miret', email: 'jaume@a.com', zones: [1], id: 1, points: 5 });
-    // this.user$ = this.mainService.getUser();
+    // this.user$ = this.userService.user$;
+    //Zones list-array that will be shown in template
     this.zonesList$ = this.listService.getZonesList();
   }
 
+  // rejectZone(zoneId: number) {
+    // this.userService.updateZones(this.userService.user.id, zoneId, 1).subscribe(() => {
+    //   this.updateUser.next(null)
+  //   });
+  // }
+
+  // addZone(zoneId: number) {
+  //   updateZones afegirà o traurà zona de user.zones segons si l'últim valor és 0 o 1 
+  //   i després actualitzarà amb post
+  //       this.userService.updateZones(this.userService.user.id, zoneId, 0).subscribe(() => {
+  //     this.updateUser.next(null)
+  //   });
+  // }
 
   ngOnInit() {
   }
