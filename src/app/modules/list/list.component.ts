83befb8585f16e2484cd8ad78631e6f6fa584dd5
Roger Miret <r.mir.min@gmail.com>
2023-06-20 17:42:19 +0200

    list-component & user-service: update updateZones()

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -3,6 +3,7 @@ import { ListService } from 'src/app/services/list.service';
 import { Observable, BehaviorSubject, of } from 'rxjs';
 import { Zone, User } from 'src/app/models/interfaces';
 import { MainService } from 'src/app/services/main.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list',
@@ -17,28 +18,28 @@ export class ListComponent implements OnInit {
   updateUser = new BehaviorSubject<User|null>(null);
   updateUser$ = this.updateUser.asObservable();
 
-  constructor(private listService: ListService, private mainService: MainService) {
+  constructor(private listService: ListService, private userService: UserService,
+    private mainService: MainService) {
     //Current user from user.service, to display user features
-    // this.user$ = this.updateUser$.pipe(switchMap(() => this.userService.getUser())); o this.userService.user$
-    this.user$ = of({ name: 'Jaume', surname: 'Miret', email: 'jaume@a.com', zones: [1], id: 1, points: 5 });
-    // this.user$ = this.userService.user$;
+    //UNA D'AQUESTES DUES OPCIONS; CREC QUE LA SEGONA, Q ÉS MÉS SIMPLE, VA BÉ SI
+    // A USERSERVICE LA FUNCIÓ UPDATEZONES ACTUALITZA L'USUARI
+    //LLAVORS TAMBÉ PODRIA SIMPLIFICAR FUNCIONS REJECTZONES I ADDZONE (TREURE EL SUBSCRIBE)
+    // this.user$ = this.updateUser$.pipe(switchMap(() => this.userService.updateUser$); o this.userService.user$
+    this.user$ = this.userService.updateUser$;
+    // this.user$ = of({ name: 'Jaume', surname: 'Miret', email: 'jaume@a.com', zones: [1], id: 1, points: 5 });
     //Zones list-array that will be shown in template
     this.zonesList$ = this.listService.getZonesList();
   }
 
-  // rejectZone(zoneId: number) {
-    // this.userService.updateZones(this.userService.user.id, zoneId, 1).subscribe(() => {
-    //   this.updateUser.next(null)
-  //   });
-  // }
+  rejectZone(zoneId: number) {
+    this.userService.updateZones(zoneId, 1)
+  }
 
-  // addZone(zoneId: number) {
+  addZone(zoneId: number) {
   //   updateZones afegirà o traurà zona de user.zones segons si l'últim valor és 0 o 1 
   //   i després actualitzarà amb post
-  //       this.userService.updateZones(this.userService.user.id, zoneId, 0).subscribe(() => {
-  //     this.updateUser.next(null)
-  //   });
-  // }
+        this.userService.updateZones(zoneId, 0)
+  }
 
   ngOnInit() {
   }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject, tap} from 'rxjs';
+import { Observable, BehaviorSubject, Subscription, tap} from 'rxjs';
 import { User } from '../models/interfaces';
 
 @Injectable({
@@ -8,8 +8,14 @@ import { User } from '../models/interfaces';
 })
 export class UserService {
 
+  
   updateUser = new BehaviorSubject<User|null>(null);
   updateUser$ = this.updateUser.asObservable();
+  //afegeixo user com a objecte perquè sigui més intuïtiu i fàcil
+  //per a la funció updateZones
+  currentUser: User | null = null;
+  subscUser: Subscription = this.updateUser$.subscribe(res => this.currentUser = res);
+
 
 
   constructor(private http: HttpClient) { }
@@ -20,5 +26,16 @@ export class UserService {
 
    register(user: any): Observable<any> {
     return this.http.post("", user);
-   }
+  }
+  
+  updateZones(zoneId: number, addOrReject: 0 | 1) { //0 to add, 1 to reject
+    if (addOrReject == 0) {
+      this.currentUser!.zones.push(zoneId);  //aquesta funció només es pot cridar amb user loggejat
+    } else {
+      let indexToRemove: number = this.currentUser!.zones.indexOf(zoneId);
+      this.currentUser!.zones.splice(indexToRemove); //idem
+    }
+    this.http.post<User>("", this.currentUser!.zones) //idem
+      .subscribe(res => { this.updateUser.next(res) });
+  }
 }
